856e00cad1ba51dd0b34cd5cdfd5019df3367c88
cascci123 <71568834+cascci123@users.noreply.github.com>
2021-04-27 14:20:15 -0700

    Add files via upload

--- solution.py
+++ solution.py
@@ -88,7 +88,8 @@ def get_route(hostname):
 
             #Fill in start
             # Make a raw socket named mySocket
-            mySocket = socket(AF_INET, SOCK_RAW, getprotobyname("icmp"))
+            icmp = getprotobyname("icmp")
+            mySocket = socket(AF_INET, SOCK_RAW, icmp)
             #Fill in end
 
 
@@ -172,3 +173,7 @@ def get_route(hostname):
             finally:
                 return tracelist2
                 mySocket.close()
+
+
+if __name__ == '__main__':
+    get_route('google.com')
